0bb319a7bcf7058ebcd13612232bd9510b33f3e5
Allan James Lapid <ajLapid718@gmail.com>
2020-06-06 13:04:43 -0400

    entire demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BasketballPlayerForm from "./BasketballPlayerForm";
+
+/*
+
+[DONE]: I want to use an external API to grab information about a basketball player;
+[DONE]: Specifically, I am concerned with two things for my project: their full name and their team name;
+
+[DONE]: I want a form that allows a user to input the first name and last name of the player;
+[DONE]:  - Let's make a separate component that will accomplish this goal;
+
+[DONE]: I want to display/render the full name of the basketball player and their team name;
+
+I want to make a cancel button
+  - This will revert us back to not just empty text inputs, but also removes the current player on the state;
+  - I'm thinking that this will involve a button, with an onClick listener, and then a handleClick method;
+
+*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasketballPlayerForm />
     </div>
   );
 }
